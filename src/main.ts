bc8aca05680a0be242eca9cec5d047341bef5618
Your Name <you@example.com>
2025-12-14 02:31:08 +0100

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure OpenAPI / Swagger documentation
+  const swagger_options = new DocumentBuilder()
+    .setTitle(process.env.API_TITLE ?? 'Example API')
+    .setDescription(process.env.API_DESCRIPTION ?? 'The API description')
+    .setVersion(process.env.API_VERSION ?? '1.0')
+    .addBearerAuth()
+    .build();
+  const swagger_document = SwaggerModule.createDocument(app, swagger_options);
+  SwaggerModule.setup('swagger/ui', app, swagger_document, {
+    yamlDocumentUrl: 'swagger/yaml',
+    jsonDocumentUrl: 'swagger/json',
+    swaggerUiEnabled: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
