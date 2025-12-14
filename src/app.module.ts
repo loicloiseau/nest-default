0e95a8449252d95480704dcc5b306c4472bbc30c
Your Name <you@example.com>
2025-12-14 02:47:44 +0100

    modified app module for logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [LoggerModule.forRoot(), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
