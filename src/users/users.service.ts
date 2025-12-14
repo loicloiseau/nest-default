9385f2dcd3231f1e4734cb3fd7082686bf1b3540
Your Name <you@example.com>
2025-12-14 01:56:11 +0100

    fix linting

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return `This action adds the new user ${JSON.stringify(createUserDto)}`;
   }
 
   findAll() {
@@ -17,7 +17,7 @@ export class UsersService {
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return `This action updates the #${id} user with ${JSON.stringify(updateUserDto)}`;
   }
 
   remove(id: number) {
