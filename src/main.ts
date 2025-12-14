6a95c530931726eaca21f776e9d2c50cc318276f
Your Name <you@example.com>
2025-12-14 02:01:48 +0100

    added todo

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  //TODO: implement global logger middleware
 }
 
 void bootstrap();
